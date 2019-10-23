255a9f40742d13face2b97b4a7719c22abde4e6c
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-10-23 14:59:51 -0500

    Add basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  @projects = []
+  (1..3).each do |x|
+    @projects << Project.find_or_create_by(slug: "user#{x}/project_#{x}")
+    User.find_or_create_by(login: "user#{x}")
+  end
+
+  @projects.each do |p|
+    @workflows = []
+    (1..3).each do |x|
+      @workflows << Workflow.find_or_create_by(project: p, display_name: "Workflow #{x}")
+    end
+  end
+
+  @workflows.each do |w|
+    (1..30).each do |x|
+      s = Subject.find_or_create_by(workflow: w, metadata: { abunch: "ofjson" })
+      Transcription.find_or_create_by(
+        workflow: w,
+        subject: s,
+        group_id: "GROUP" + rand(3).to_s,
+        status: rand(3),
+        flagged: [true, false].sample,
+        text: { abunch: "ofjson" })
+    end
+  end
+end
