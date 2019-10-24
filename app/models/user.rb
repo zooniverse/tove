0efcdbee92344579be745629109e8ef1d4e91aef
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-10-23 19:23:55 -0500

    Add User#from_jwt class method and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
   attr_accessor :display_name, :admin, :roles
   validates :login, presence: true
+
+  def self.from_jwt(data = { })
+    id, login = data&.values_at 'id', 'login'
+    return unless id && login
+
+    User.where(id: id, login: login).first_or_create do |user|
+      user.display_name = data['dname']
+      user.admin = data['admin'] == true # explicit
+    end
+  end
+
+  def roles
+    @roles || { }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Subject, type: :model do
       expect(build(:user, login: nil)).to_not be_valid
     end
   end
+
+  describe '#from_jwt' do
+    it 'creates a valid user with the correct attributes' do
+      data = { 'id' => 9999, 'login' => "username" }
+      expect {
+        User.from_jwt(data)
+      }.to change {User.count}.by(1)
+      expect(User.find(9999)).to be_valid
+    end
+  end
 end
