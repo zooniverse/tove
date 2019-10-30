869154613641f66db438f08e8b61b076c1f50b10
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-10-30 16:12:39 -0500

    Bonus spec

--- spec/models/transcription_spec.rb
+++ spec/models/transcription_spec.rb
@@ -15,5 +15,9 @@ RSpec.describe Transcription, type: :model do
     it 'requires text' do
       expect(build(:transcription, text: nil)).to_not be_valid
     end
+
+    it 'allows empty text' do
+      expect(create(:transcription, text: {})).to be_valid
+    end
   end
 end
