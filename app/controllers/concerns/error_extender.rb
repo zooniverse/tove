b726cd2a574e7b40be41584cdaacaca95676a281
Zach Wolfenbarger <zach@zooniverse.org>
2019-10-31 19:19:55 -0500

    Abandon strong parameters (#47)
    * Abandon strong params for now
    
    * Catch invalid attr errors with a 422

--- app/controllers/concerns/error_extender.rb
+++ app/controllers/concerns/error_extender.rb
@@ -4,10 +4,21 @@ module ErrorExtender
 
   included do
     rescue_from ActionController::BadRequest, with: :render_jsonapi_bad_request
+    rescue_from ActiveModel::UnknownAttributeError, with: :render_jsonapi_unknown_attribute
   end
 
   def render_jsonapi_bad_request(exception)
     error = { status: '400', title: Rack::Utils::HTTP_STATUS_CODES[400] }
     render jsonapi_errors: [error], status: :bad_request
+  end
+
+  def render_jsonapi_unknown_attribute(exception)
+    error = {
+      status: '422',
+      title: Rack::Utils::HTTP_STATUS_CODES[422],
+      detail: exception.to_param
+    }
+
+    render jsonapi_errors: [error], status: :unprocessable_entity
   end
 end
