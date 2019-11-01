9254f4431f4fd3b2b7ae886066f6aeeaa3b30431
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-10-31 19:03:06 -0500

    Catch invalid attr errors with a 422

--- spec/controllers/transcriptions_controller_spec.rb
+++ spec/controllers/transcriptions_controller_spec.rb
@@ -105,6 +105,12 @@ RSpec.describe TranscriptionsController, type: :controller do
         patch :update, params: busted_params
         expect(response).to have_http_status(:bad_request)
       end
+
+      it 'contains an invalid attribute' do
+        busted_params = { id: transcription.id, "data": { "type": "transcriptions", "attributes": { "garf": true } } }
+        patch :update, params: busted_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
