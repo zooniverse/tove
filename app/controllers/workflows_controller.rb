be31bb4e1ffd51988260bd887832fc6726ae72a4
Will Granger <twgranger@gmail.com>
2019-11-16 17:30:37 -0600

    Add Workflow Show Method

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,7 +1,14 @@
 class WorkflowsController < ApplicationController
+  include JSONAPI::Deserialization
+
   def index
     @workflows = Workflow.all
     jsonapi_render(@workflows, allowed_filters)
+  end
+
+  def show
+    @workflow = Workflow.find(params[:id])
+    render jsonapi: @workflow
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :show]
-  resources :workflows, only: [:index]
+  resources :workflows, only: [:index, :show]
   resources :transcriptions, only: [:index, :show, :update]
 end
