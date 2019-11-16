03c6f71e8bc66bc0d14deca6074fce2b827945f5
Will Granger <twgranger@gmail.com>
2019-11-16 17:22:42 -0600

    Add Project Show Method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,14 @@
 class ProjectsController < ApplicationController
+  include JSONAPI::Deserialization
+
   def index
     @projects = Project.all
     jsonapi_render(@projects, allowed_filters)
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    render jsonapi: @project
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :projects, only: [:index]
+  resources :projects, only: [:index, :show]
   resources :workflows, only: [:index]
   resources :transcriptions, only: [:index, :show, :update]
 end
