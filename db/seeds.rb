9c65607e1e24779beec2e0ce5ac66c44972cbd6d
Nicole Ciemniak <nciemniak@A0000000ST.local>
2019-12-12 14:26:36 -0600

    revert changes relating to seed file issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,27 @@
 ActiveRecord::Base.transaction do
-  if Project.count < 3  # assume we've already seeded if we have 3+ projects
-    @projects = []
+  @projects = []
+  (1..3).each do |x|
+    @projects << Project.find_or_create_by(slug: "user#{x}/project_#{x}")
+    User.find_or_create_by(login: "user#{x}")
+  end
+
+  @projects.each do |p|
+    @workflows = []
     (1..3).each do |x|
-      @projects << Project.create(slug: "user#{x}/project_#{x}")
-      User.create(login: "user#{x}")
+      @workflows << Workflow.find_or_create_by(project: p, display_name: "Workflow #{x}")
     end
-  
-    @projects.each do |p|
-      @workflows = []
-      (1..3).each do |x|
-        @workflows << Workflow.create(project: p, display_name: "Workflow #{x}")
-      end
-    end
-  
-    @workflows.each do |w|
-      (1..30).each do |x|
-        s = Subject.create(workflow: w, metadata: { abunch: "ofjson" })
-        Transcription.create(
-          workflow: w,
-          subject: s,
-          group_id: "GROUP" + rand(3).to_s,
-          status: rand(3),
-          flagged: [true, false].sample,
-          text: { abunch: "ofjson" })
-      end
+  end
+
+  @workflows.each do |w|
+    (1..30).each do |x|
+      s = Subject.find_or_create_by(workflow: w, metadata: { abunch: "ofjson" })
+      Transcription.find_or_create_by(
+        workflow: w,
+        subject: s,
+        group_id: "GROUP" + rand(3).to_s,
+        status: rand(3),
+        flagged: [true, false].sample,
+        text: { abunch: "ofjson" })
     end
   end
 end
