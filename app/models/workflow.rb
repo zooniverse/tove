a5ab9840c79a63051b51e12566c6c0e3f8627f50
Nicole Ciemniak <nciemniak@A0000000ST.local>
2019-12-13 10:41:00 -0600

    add new attributes to workflow

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -6,6 +6,22 @@ class Workflow < ApplicationRecord
   validates :display_name, presence: true
 
   def groups
-    transcriptions.group(:group_id).count
+    transcriptions_per_group = transcriptions.group(:group_id).count
+    # total_count = transcriptions_per_group.values.reduce(:+)
+    group_count = transcriptions_per_group.count
+
+    {
+      transcriptions_per_group: transcriptions_per_group,
+      # total_transcriptions: total_count,
+      group_count: group_count
+    }
+  end
+
+  def total_transcriptions
+    transcriptions.count
+  end
+
+  def approved_transcriptions
+    transcriptions.count { |t| t.status == 'approved' }
   end
 end
