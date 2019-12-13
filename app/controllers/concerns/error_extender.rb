ad4479079df10e049c1133d7e4f29d593ff43399
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-12-13 16:57:56 -0600

    Add token expired error

--- app/controllers/concerns/error_extender.rb
+++ app/controllers/concerns/error_extender.rb
@@ -5,11 +5,17 @@ module ErrorExtender
   included do
     rescue_from ActionController::BadRequest, with: :render_jsonapi_bad_request
     rescue_from ActiveModel::UnknownAttributeError, with: :render_jsonapi_unknown_attribute
+    rescue_from Panoptes::Client::AuthenticationExpired, with: :render_jsonapi_token_expired
   end
 
   def render_jsonapi_bad_request(exception)
     error = { status: '400', title: Rack::Utils::HTTP_STATUS_CODES[400] }
     render jsonapi_errors: [error], status: :bad_request
+  end
+
+  def render_jsonapi_token_expired(exception)
+    error = { status: '401', title: Rack::Utils::HTTP_STATUS_CODES[401] }
+    render jsonapi_errors: [error], status: :unauthorized
   end
 
   def render_jsonapi_unknown_attribute(exception)
