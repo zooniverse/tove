369c5504cad589db8487b8d18aae35dd099fe066
Nicole Ciemniak <nciemniak@A0000000ST.local>
2019-12-16 14:34:21 -0600

    tweak file to avoid needing project count check

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,31 @@
 ActiveRecord::Base.transaction do
-  if Project.count < 3  # assume we've already seeded if we have 3+ projects
-    @projects = []
+  @projects = []
+  (1..3).each do |x|
+    curr_project = Project.create(slug: "user#{x}/project_#{x}")
+    @projects << curr_project if curr_project.valid?
+
+    User.create(login: "user#{x}")
+  end
+
+  @workflows = []
+  @projects.each do |p|
+    @workflows = []
     (1..3).each do |x|
-      @projects << Project.create(slug: "user#{x}/project_#{x}")
-      User.create(login: "user#{x}")
+      @workflows << Workflow.create(project: p, display_name: "Workflow #{x}")
     end
-  
-    @projects.each do |p|
-      @workflows = []
-      (1..3).each do |x|
-        @workflows << Workflow.create(project: p, display_name: "Workflow #{x}")
-      end
-    end
-  
-    @workflows.each do |w|
-      (1..30).each do |x|
-        s = Subject.create(workflow: w, metadata: { abunch: "ofjson" })
-        Transcription.create(
-          workflow: w,
-          subject: s,
-          group_id: "GROUP" + rand(3).to_s,
-          status: rand(3),
-          flagged: [true, false].sample,
-          text: { abunch: "ofjson" })
-      end
+  end
+
+  @workflows.each do |w|
+    (1..30).each do |_x|
+      s = Subject.create(workflow: w, metadata: { abunch: 'ofjson' })
+      Transcription.create(
+        workflow: w,
+        subject: s,
+        group_id: 'GROUP' + rand(3).to_s,
+        status: rand(3),
+        flagged: [true, false].sample,
+        text: { abunch: 'ofjson' }
+      )
     end
   end
 end
