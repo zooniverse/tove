a36b9178578b1bd43fc24ee6ebcf608ba33cc5fb
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-12-17 13:44:38 -0600

    Refactor to use built-in authentication in panoptes client gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,21 +4,4 @@ RSpec.describe Subject, type: :model do
       expect(build(:user, login: nil)).to_not be_valid
     end
   end
-
-  describe '#from_jwt' do
-    it 'creates a valid user with the correct attributes' do
-      data = { 'id' => 9999, 'login' => "username" }
-      expect {
-        User.from_jwt(data)
-      }.to change {User.count}.by(1)
-      expect(User.find(9999)).to be_valid
-    end
-
-    it 'has an invalid JWT' do
-      data = { }
-      expect {
-        User.from_jwt(data)
-      }.to raise_error ArgumentError
-    end
-  end
 end
