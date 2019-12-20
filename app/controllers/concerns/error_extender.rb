6d14402e692122ae8fe8cbb694f049038d379d56
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-12-20 13:00:12 -0600

    Override gem rescue_from on StandardError to report to Sentry

--- app/controllers/concerns/error_extender.rb
+++ app/controllers/concerns/error_extender.rb
@@ -5,6 +5,21 @@ module ErrorExtender
   included do
     rescue_from ActionController::BadRequest, with: :render_jsonapi_bad_request
     rescue_from ActiveModel::UnknownAttributeError, with: :render_jsonapi_unknown_attribute
+    rescue_from StandardError, with: :report_to_sentry
+  end
+
+  def report_to_sentry(exception)
+    if current_user
+      Raven.user_context(
+        id: current_user.id,
+        username: current_user.login,
+        roles: current_user.roles,
+
+      )
+    end
+
+    Raven.capture_exception(exception)
+    render_jsonapi_internal_server_error(exception)
   end
 
   def render_jsonapi_bad_request(exception)
