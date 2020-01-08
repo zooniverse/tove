6639df6cba239a42a8e238313b87247a8443f289
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-08 14:39:24 -0600

    remove the sneaky db columns *the right way*

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_174058) do
+ActiveRecord::Schema.define(version: 2020_01_06_225502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
