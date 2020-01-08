381fedfd9ef193abc2df05f22e270a3a44419535
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-08 11:28:50 -0600

    add method to translate enum term to int on param

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -1,6 +1,8 @@
 class TranscriptionsController < ApplicationController
   include JSONAPI::Deserialization
 
+  before_action :status_filter_to_int, only: :index
+
   def index
     @transcriptions = Transcription.all
     jsonapi_render(@transcriptions, allowed_filters)
@@ -22,6 +24,19 @@ class TranscriptionsController < ApplicationController
 
   def update_attrs
     jsonapi_deserialize(params)
+  end
+
+  # jsonapi.rb filtering doesn't handle filtering by enum term (e.g. 'ready'),
+  # so we must translate status terms to their integer value if they're present
+  def status_filter_to_int
+    if params[:filter]
+      params[:filter].each do |key, value|
+        if key.split('_').first == 'status'
+          enum_value = Transcription.statuses[value]
+          params[:filter][key] = enum_value if enum_value
+        end
+      end
+    end
   end
 
   def type_invalid?
