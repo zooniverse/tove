5fe7d268af29468fa09bc0db8f560979326a94f1
Campbell Allen <campbell.allen@gmail.com>
2020-01-10 14:59:14 +0000

    use different method name to serializer attribute

--- app/serializers/workflow_serializer.rb
+++ app/serializers/workflow_serializer.rb
@@ -1,6 +1,7 @@
 class WorkflowSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :display_name, :groups, :total_transcriptions
+  attributes :display_name, :total_transcriptions
+  attribute :groups, &:transcription_group_data
   belongs_to :project
 end
