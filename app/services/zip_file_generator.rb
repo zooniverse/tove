44fd4f2b8db64a7fd9211c0824001145d693e61b
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-16 15:20:52 -0600

    add zip file generator class

---
+++ app/services/zip_file_generator.rb
@@ -0,0 +1,11 @@
+class ZipFileGenerator
+  # Initialize with the directory to zip and the location of the output archive.
+  def initialize(input_dir, output_file)
+    @input_dir = input_dir
+    @output_file = output_file
+  end
+
+  def write_to_file
+    # TO DO
+  end
+end
