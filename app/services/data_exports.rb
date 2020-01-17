3d93d29a8ba958b0845a3fdc42484cf59a55c7b8
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-17 17:24:01 -0600

    sketching out TranscripFileGen methods

--- app/services/data_exports.rb
+++ app/services/data_exports.rb
@@ -5,14 +5,21 @@ module DataExports
     def generate_transcription_files(transcription_id)
       transcription = Transcription.find(transcription_id)
 
-      raw_transcription_file = Tempfile.new([transcription_id.to_s, '.json'])
-      raw_transcription_file.write(transcription.text)
+      # raw_transcription_file = Tempfile.new([transcription_id.to_s, '.json'])
+      # raw_transcription_file.write(transcription.text)
+
+      path = File.expand_path('~/Documents/temp')
+      filename = File.join(path,'/hello.json')
+
+      File.open(filename, 'w') { |f|
+        f.puts transcription.text
+      }
 
       raw_transcription_file_path = File.join(generate_path(transcription), 'raw-transcription-data.json')
       azure = AzureBlobStorage.new
-      azure.put_file(raw_transcription_file_path, raw_transcription_file)
+      azure.put_file(raw_transcription_file_path, filename)
 
-      raw_transcription_file.unlink
+      # raw_transcription_file.unlink
     end
 
     def generate_path(transcription)
