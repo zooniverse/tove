262abf9d480534fbc1c953dbb63a1a3f84949cdc
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-17 17:33:05 -0600

    flesh out TranFileGen methods

--- app/services/data_exports.rb
+++ app/services/data_exports.rb
@@ -3,13 +3,15 @@ require 'tempfile'
 module DataExports
   class TranscriptionFileGenerator
     def generate_transcription_files(transcription_id)
-      transcription = Transcription.find(transcription_id)
+      @transcription = Transcription.find(transcription_id)
 
+      # really wanted to do this using the Tempfile class, but it didn't work
+      # when I passed the tempfile to azure
       # raw_transcription_file = Tempfile.new([transcription_id.to_s, '.json'])
       # raw_transcription_file.write(transcription.text)
 
-      path = File.expand_path('~/Documents/temp')
-      filename = File.join(path,'/hello.json')
+      directory_path = File.expand_path('~/Documents/temp')
+      filename = File.join(path,'/t.json')
 
       File.open(filename, 'w') { |f|
         f.puts transcription.text
@@ -20,6 +22,15 @@ module DataExports
       azure.put_file(raw_transcription_file_path, filename)
 
       # raw_transcription_file.unlink
+    end
+
+    def raw_transcription_file(directory_path)
+      file_path = File.join(path, "raw_data_#{@transcription.id}.json")
+      
+      File.open(file_path, 'w') { |f|
+        f.puts @transcription.text
+      }
+      file_path
     end
 
     def generate_path(transcription)
