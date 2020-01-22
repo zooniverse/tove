dcd107278052ce8edad871aec9823eb72fd3866e
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-22 12:17:09 -0600

    delete unused spec for now bc its breaking build

--- spec/services/data_exports_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe TranscriptionFileGenerator, type: :service do
-  describe '#generate_transcription_files' do
-    let(:transcription){ create :transcription }
-
-    it 'does something' do
-      
-    end
-  end
-end
