306f0fa14e192ecf7a882c60fc7326023b2f3d74
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-22 11:57:57 -0600

    generating files and exporting to azure

--- spec/services/data_exports_spec.rb
+++ spec/services/data_exports_spec.rb
@@ -1,2 +1,9 @@
 RSpec.describe TranscriptionFileGenerator, type: :service do
+  describe '#generate_transcription_files' do
+    let(:transcription){ create :transcription }
+
+    it 'does something' do
+      
+    end
+  end
 end
