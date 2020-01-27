054e68e5d00c1cb60dac41b8e2d6f2c06ad14492
Nicole Ciemniak <nciemniak@gmail.com>
2020-01-27 14:27:43 -0600

    add route for export

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   resources :projects, only: [:index, :show]
   resources :workflows, only: [:index, :show]
-  resources :transcriptions, only: [:index, :show, :update]
+  resources :transcriptions, only: [:index, :show, :update] do
+    get 'export', on: :member
+  end
+
 end
