171ab51fed93915427162ba46e5f759ad0e67190
Zach Wolfenbarger <zach@zooniverse.org>
2020-01-28 10:06:14 -0600

    Turn off deep munging so Rails stops stripping nulls out of JSON payloads (#108)

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Tove
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_dispatch.perform_deep_munge = false
   end
 end
