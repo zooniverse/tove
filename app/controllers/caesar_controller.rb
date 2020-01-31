f69dd8d56f724efc8253b6e1b39034bb7f13645f
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2020-01-31 17:42:48 -0600

    Add new controller, spec, fixture, and route

---
+++ app/controllers/caesar_controller.rb
@@ -0,0 +1,28 @@
+class CaesarController < ActionController::Base
+  include JSONAPI::Errors
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def import
+    begin
+      importer = CaesarImporter.new(arg_hash)
+      importer.process
+    rescue => exception
+      Raven.capture_exception(exception)
+      error = { status: '400', title: Rack::Utils::HTTP_STATUS_CODES[400] }
+      render jsonapi_errors: [error], status: :bad_request
+    else
+      head :no_content
+    end
+  end
+
+  private
+
+  def arg_hash
+    {
+      id: params[:id],
+      reducible: params[:reducible],
+      data: params[:data],
+      subject: params[:subject]
+    }
+  end
+end
---
+++ spec/controllers/caesar_controller_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe CaesarController, type: :controller do
+
+  describe '#import' do
+    it 'instantiates an importer and calls #process' do
+      expect(CaesarImporter).to receive(:new).and_call_original
+      expect_any_instance_of(CaesarImporter).to receive(:process)
+      post :import, as: :json, body: File.read(Rails.root.join("spec/fixtures/caesar_payload.json"))
+    end
+
+    it 'returns a 204 if processing is successful' do
+      expect{
+        post :import, as: :json, body: File.read(Rails.root.join("spec/fixtures/caesar_payload.json"))
+      }
+      .to change{Project.count}.by(1)
+      .and change{Workflow.count}.by(1)
+      .and change{Transcription.count}.by(1)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns a 422 if there is an error' do
+      expect(Raven).to receive(:capture_exception)
+      post :import, as: :json, body: {just: 'garbage'}.to_json
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+end
