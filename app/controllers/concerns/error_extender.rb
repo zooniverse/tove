1d92c276b949a6d9ca1475e69bcaf3c9f90af3aa
Will Granger <twgranger@gmail.com>
2020-02-06 15:30:31 -0600

    Remove Unnecessary Unknown Attribute Extender

--- app/controllers/concerns/error_extender.rb
+++ app/controllers/concerns/error_extender.rb
@@ -4,7 +4,6 @@ module ErrorExtender
 
   included do
     rescue_from ActionController::BadRequest, with: :render_jsonapi_bad_request
-    rescue_from ActiveModel::UnknownAttributeError, with: :render_jsonapi_unknown_attribute
     rescue_from Panoptes::Client::AuthenticationExpired, with: :render_jsonapi_token_expired
     rescue_from Pundit::NotAuthorizedError, with: :render_jsonapi_not_authorized
     rescue_from ActionDispatch::Http::Parameters::ParseError, with: :render_jsonapi_bad_request
@@ -35,16 +34,6 @@ module ErrorExtender
   def render_jsonapi_token_expired(exception)
     error = { status: '401', title: Rack::Utils::HTTP_STATUS_CODES[401] }
     render jsonapi_errors: [error], status: :unauthorized
-  end
-
-  def render_jsonapi_unknown_attribute(exception)
-    error = {
-      status: '422',
-      title: Rack::Utils::HTTP_STATUS_CODES[422],
-      detail: exception.to_param
-    }
-
-    render jsonapi_errors: [error], status: :unprocessable_entity
   end
 
   def render_jsonapi_not_authorized
