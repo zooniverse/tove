eeb36316d43e72c9833588046726aa11af3fa400
Nicole Ciemniak <nciemniak@gmail.com>
2020-02-07 10:33:16 -0600

    zip generator spec: remove file created by test

--- spec/services/zip_file_generator_spec.rb
+++ spec/services/zip_file_generator_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe ZipFileGenerator do
   let(:zip_file_path) { Rails.root.join("spec/fixtures/files/test-zip.zip") }
 
-  describe '#write' do
+  describe '#write', :focus => true do
     context 'when given a multi-level directory' do
       let(:zip_generator) {
         described_class.new(Rails.root.join("spec/fixtures"), zip_file_path)
@@ -10,6 +10,7 @@ RSpec.describe ZipFileGenerator do
       it 'generates a zip file' do
         zip_generator.write
         expect(File).to exist(zip_file_path)
+        File.delete(zip_file_path)
       end
     end
   end
