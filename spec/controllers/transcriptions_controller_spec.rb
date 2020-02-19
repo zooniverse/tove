e90582d79e4e1118a243c9b57772a2d30a22f36e
Nicole Ciemniak <nciemniak@gmail.com>
2020-02-19 13:51:06 -0600

    place serializer tests in their own spec file

--- spec/controllers/transcriptions_controller_spec.rb
+++ spec/controllers/transcriptions_controller_spec.rb
@@ -108,17 +108,6 @@ RSpec.describe TranscriptionsController, type: :controller do
   describe '#show' do
     let!(:transcription) { create(:transcription) }
 
-    it 'serializes the expected attributes' do
-      get :show, params: { id: transcription.id }
-      expect(json_data['attributes']).to have_key('workflow_id')
-      expect(json_data['attributes']).to have_key('group_id')
-      expect(json_data['attributes']).to have_key('text')
-      expect(json_data['attributes']).to have_key('status')
-      expect(json_data['attributes']).to have_key('flagged')
-      expect(json_data['attributes']).to have_key('updated_at')
-      expect(json_data['attributes']).to have_key('updated_by')
-    end
-
     describe 'roles' do
       before do
         allow(controller).to receive(:current_user).and_return user
