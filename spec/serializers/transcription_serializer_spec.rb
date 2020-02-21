035633cceffb741b4e700411aa76a6f229c0d796
Nicole Ciemniak <nciemniak@gmail.com>
2020-02-21 16:35:29 -0600

    modify transcription serializer spec

--- spec/serializers/transcription_serializer_spec.rb
+++ spec/serializers/transcription_serializer_spec.rb
@@ -2,23 +2,24 @@ RSpec.describe TranscriptionSerializer, type: :serializer do
   let(:transcription) { build(:transcription) }
 
   let(:serializer) { described_class.new(transcription) }
-  let(:serialized_data) { serializer.to_hash[:data][:attributes] }
+  let(:data) { serializer.to_hash[:data][:attributes] }
 
-  let(:collection_serializer) {
-    described_class.new(transcription, { params: { is_collection: true }})
+  let(:serializer_include_text) {
+    described_class.new(transcription, { params: { serialize_text: true }})
   }
-  let(:serialized_collection) { collection_serializer.to_hash[:data][:attributes] }
+  let(:data_with_text) { serializer_include_text.to_hash[:data][:attributes] }
 
   it 'serializes the expected attributes' do
-    expect(serialized_data).to have_key(:workflow_id)
-    expect(serialized_data).to have_key(:group_id)
-    expect(serialized_data).to have_key(:status)
-    expect(serialized_data).to have_key(:flagged)
-    expect(serialized_data).to have_key(:updated_at)
-    expect(serialized_data).to have_key(:updated_by)
+    expect(data_with_text).to have_key(:workflow_id)
+    expect(data_with_text).to have_key(:group_id)
+    expect(data_with_text).to have_key(:status)
+    expect(data_with_text).to have_key(:flagged)
+    expect(data_with_text).to have_key(:updated_at)
+    expect(data_with_text).to have_key(:updated_by)
+    expect(data_with_text).to have_key(:text)
   end
 
-  it 'doesnt serialize text when is_collection param is true' do
-    expect(serialized_collection).not_to have_key(:text)
+  it 'doesnt serialize text when serialize_text is not set' do
+    expect(data).not_to have_key(:text)
   end
 end
