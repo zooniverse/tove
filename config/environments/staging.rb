0137dc248d1bba6776199c2879ff0541d3cd9fe3
nciemniak <nciemniak@gmail.com>
2020-03-16 11:38:50 -0500

    Update storage account name (#161)
    * use new azure storage account 'tove'

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on azure (see config/storage.yml for options).
-  config.active_storage.service = :azure
+  config.active_storage.service = :staging
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
