7771905b289dbb10bfcc0b110a7f8c99fa912970
Zach Wolfenbarger <zach@zooniverse.org>
2020-07-01 12:01:50 -0500

    Catch and release RecordNotUnique errors (#207)

--- app/controllers/caesar_controller.rb
+++ app/controllers/caesar_controller.rb
@@ -11,21 +11,16 @@ class CaesarController < ActionController::Base
         subject: params[:subject]
       )
       importer.process
-    rescue => exception
+    rescue ActiveRecord::RecordNotUnique => exception
       logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
       logger.tagged('DUP_ID') { logger.warn exception }
-
-      Raven.capture_exception(exception) if report_error_with_rate_limit
+      head :no_content
+    rescue => exception
+      Raven.capture_exception(exception)
       error = { status: '400', title: Rack::Utils::HTTP_STATUS_CODES[400] }
       render jsonapi_errors: [error], status: :bad_request
     else
       head :no_content
     end
-  end
-
-  private
-
-  def report_error_with_rate_limit
-    rand(100) == 1
   end
 end
